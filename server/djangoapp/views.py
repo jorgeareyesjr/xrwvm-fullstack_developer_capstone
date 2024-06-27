ef7741bec75c149da1955fa8863e80626c359bd7
Jorge A. Reyes Jr. <10119138+jorgeareyesjr@users.noreply.github.com>
2024-06-27 11:40:01 -0700

    Add a new register view to handler a registration request; Clean up view comments

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -19,47 +19,88 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
+"""
+Note on the `login_user` and `logout_request` views:
+The `login_user` and `logout_request` views are implemented as function-based views.
+These views handle the sign-in and sign-out requests from the frontend, respectively.
+In both, the current users' authentication details are saved in the window.sessionStorage object.
+Once in the session storage, the frontend can access the user's authentication status using the `checkSession()` function.
+"""
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
+# When the user logs in:
 def login_user(request):
-    # Get username and password from request.POST dictionary
+    # Deserialize the request body to a data dictionary
     data = json.loads(request.body)
+    # Extract the username and password from the data dictionary
     username = data['userName']
     password = data['password']
-    # Try to check if provide credential can be authenticated
+    # Try to authenticate user
     user = authenticate(username=username, password=password)
-    # Assign the username to `data`, overriding the previous value of `data`
+    # Reassign the data dictionary with the username
     data = {"userName": username}
     if user is not None:
         # If user is valid, call login method to login current user
         login(request, user)
-        # Add the username authentication status to `data`
+        # Add the authentication status to the data dictionary
         data = {"userName": username, "status": "Authenticated"}
-        # Return a JSON response, the username and authentication status, to the frontend
-        """
-        The frontend will store this returned data to the browser window.sessionStorage.
-        Once the window.sessionStorage data is set, the frontend now has the username and authentication status ready for use.
-        The frontend uses `checkSession` to check if the user is authenticated.
-        """
+        # Return a JSON response, the user data dictionary, to the frontend
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
+# When the user logs out:
 def logout_request(request):
     # Logout user
     logout(request)
     data = {"userName":""}
-    # Return a JSON response, an empty username and omitted authentication status, to the frontend
-    """
-    The frontend will clear the window.sessionStorage username data once the JSON response is received.
-     The frontend uses `checkSession` to check if the user is authenticated.
-    """
+    # Return a JSON response, a user data dictionary, to the frontend
     return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
+@csrf_exempt
+# When the user registers:
+def registration(request):
+    # Deserialize the request body to a data dictionary
+    data = json.loads(request.body)
+    # Extract the user data from the data dictionary
+    first_name = data['firstName']
+    last_name = data['lastName']
+    email = data['email']
+    username = data['userName']
+    password = data['password']
+
+    # Check if the username already exists
+    username_already_exists = False
+    if User.objects.filter(username=username).exists():
+        username_already_exists = True
+    else:
+        # If the username does not already exist, log the username, for debugging purposes
+        logger.debug("{} is new user".format(username))
+
+    # If this is a new username
+    if not username_already_exists:
+        # Create a new user and save to the database
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            username=username,
+            password=password,
+        )
+        # Log the newly created user in, following the normal login process
+        login(request, user)
+        # Reassign the data dictionary with the username and authentication status
+        data = {"userName":username,"status":"Authenticated"}
+        # Return a JSON response, the user data dictionary, to the frontend
+        return JsonResponse(data)
+    
+    # If the username already exists, prevent a duplicate registration
+    else:
+        # Reassign the data dictionary with the username and an error message
+        data = {"userName":username, "error":"Already Registered"}
+        # Return a JSON response, the user data dictionary, to the frontend
+        return JsonResponse(data)
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
