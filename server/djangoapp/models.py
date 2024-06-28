b8b3222faa446d95a05d7463a182d23cfd919e27
Jorge A. Reyes Jr. <10119138+jorgeareyesjr@users.noreply.github.com>
2024-06-27 20:56:12 -0700

    Create new django models for CarMake and CarModel

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,75 @@
-# Uncomment the following imports before adding the Model code
-
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(
+        null=False,
+        max_length=50,
+        default=None,
+    )
+    description = models.TextField(
+        null=False,
+        max_length=1000,
+        default=None,
+    )
 
+    # Create a toString method for the class for object string representation
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    # Many-To-One relationship to Car Make model
+    # One Car Make has many Car Models, using ForeignKey field
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(
+        null=False,
+        max_length=50,
+        default=None,
+    )
+    # Types
+    COUPE = 'Coupe'
+    HATCHBACK = 'Hatchback'
+    MICRO = 'Micro'
+    ROADSTER = 'Roadster'
+    SEDAN = 'Sedan'
+    SPORTCAR = 'Sportcar'
+    SUV = 'SUV'
+    PICKUP = 'Pickup'
+    WAGON = 'WAGON'
+    VAN = 'Van'
+    # Type choices
+    TYPE_CHOICES = [
+        (COUPE, 'Coupe'),
+        (HATCHBACK, 'Hatchback'),
+        (MICRO, 'Micro'),
+        (ROADSTER, 'Roadster'),
+        (SEDAN, 'Sedan'),
+        (SPORTCAR, 'Sportcar'),
+        (SUV, 'SUV'),
+        (PICKUP, 'Pickup'),
+        (WAGON, 'WAGON'),
+        (VAN, 'Van'),
+    ]
+    # Type (CharField with a choices argument to provide limited choices)
+    type = models.CharField(
+        null=False,
+        max_length=50,
+        choices=TYPE_CHOICES,
+        default=None,
+    )
+    year = models.IntegerField(
+        null=False,
+        validators=[
+            MinValueValidator(2000),
+            MaxValueValidator(2025)
+        ],
+        default=2024,
+    )
+    dealer_id = models.IntegerField(
+        null=False,
+        default=None,
+    )
